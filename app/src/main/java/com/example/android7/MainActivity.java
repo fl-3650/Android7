f6a3164af0333ee1976c4cc40e8e38d8d373cf1f
fl_3650 <repilovi@gmail.com>
2024-03-25 13:29:52 +0300

    AlertDialog done

--- app/src/main/java/com/example/android7/MainActivity.java
+++ app/src/main/java/com/example/android7/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.android7;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -17,6 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent startIntent = new Intent(this, MusicService.class);
         startService(startIntent);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Confirmation");
+        builder.setMessage("Sure?");
+        builder.setIcon(R.drawable.ic_dialog_alert);
+
+        builder.setPositiveButton("Yes", (dialog, which) -> {
+
+        });
+
+        builder.setNegativeButton("Cancel", (dialog, which) -> {
+            dialog.dismiss();
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     @Override
